645eb166b7d56b02027791d157b71d3cd8d99f6f
Paul Haddad <paulh16@gmail.com>
2016-06-02 07:49:39 -0500

    Add additional tests for Colorizr.create_colors

--- spec/lib/colorizr_spec.rb
+++ spec/lib/colorizr_spec.rb
@@ -1,6 +1,22 @@
 require "./lib/colorizr"
 
 describe Colorizr do
+  describe ".create_colors" do
+    it "dynamically creates color methods" do
+      Colorizr.create_colors
+
+      expect(String.instance_methods).to include(:red)
+      expect(String.instance_methods).to include(:green)
+      expect(String.instance_methods).to include(:yellow)
+      expect(String.instance_methods).to include(:blue)
+      expect(String.instance_methods).to include(:pink)
+      expect(String.instance_methods).to include(:light_blue)
+      expect(String.instance_methods).to include(:white)
+      expect(String.instance_methods).to include(:light_grey)
+      expect(String.instance_methods).to include(:black)
+    end
+  end
+
   describe "#blue" do
     it "makes the string blue" do
       text = "My new string"
